8bdec3392012a39eccdd42f810a0a61dc8035535
Franek Borowiec <franek.borowiec@gmail.com>
2020-11-25 00:00:31 +0100

    Added database to project and query for height

---
+++ database_webapp/email_sender.py
@@ -0,0 +1,7 @@
+import email
+
+
+class EmailSender:
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
--- database_webapp/main.py
+++ database_webapp/main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from email_sender import EmailSender
+
 
 app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:@localhost/height_webapp_db"
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
 
 
 @app.route("/")
@@ -13,9 +31,20 @@ def input():
     if request.method == "POST":
         email = request.form["email_name"]
         height = request.form["height_name"]
-    return render_template("input.html")
+
+        es = EmailSender(email, height)
+
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("input.html")
+        return render_template(
+            "index.html", text="Your email address is already stored!"
+        )
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.debug = True
     app.run()
