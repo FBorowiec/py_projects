bfc74387963532a51b83f38c7ba69b5e7de8ce6c
Franek Borowiec <franek.borowiec@gmail.com>
2020-11-24 21:26:07 +0100

    Website for the webapp

---
+++ database_webapp/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/database_webapp/templates/input", methods=["POST"])
+def input():
+    if request.method == "POST":
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+    return render_template("input.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
